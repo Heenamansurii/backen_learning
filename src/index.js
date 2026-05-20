4cd2ed8ddc2fb1f480b186b9b09a0c2a8e191e87
Heenamansurii <heenamansuri318@gmail.com>
2026-05-20 13:43:21 +0530

    util undastanding

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import express from "express";
+const app = express()
+;(async() => {
+    try{
+
+        mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`,)
+       app.on("error", (error) => {
+            console.error("ERROR: ",error)
+            throw err
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port $
+                {process.env.PORT}`);
+         })
+
+        
+
+    }catch(error){
+        console.error("ERROR: ",error)
+        throw err
+        
+    }   
+})()*/

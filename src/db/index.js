28bc835c5ca21b84aa283ba22ced5a4a52384fdb
Heenamansurii <heenamansuri318@gmail.com>
2026-05-20 17:12:13 +0530

    model structure

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
-import {DB_NAME} from "./constants";
+import {DB_NAME} from "../constants.js";
+
+
+
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\nMongoDB connected: ${connectionInstance.connection.host}`);
 
     } catch (error) {
@@ -11,3 +14,35 @@ const connectDB = async () => {
     }
 }
 export default connectDB
+
+
+
+
+
+
+/*****************2nd way to connect to DB *********************
+import express from "express";
+const app = express()
+(async() => {
+    try{
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+       app.on("error", (error) => {
+            console.log("ERROR: ",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port $
+                {process.env.PORT}`);
+         })
+
+     }    catch(error){
+        console.error("ERROR: ",error)
+        throw error
+          }
+        })()*/
+
+
+/***************1st way to connect to DB *********************
+fuction connectDB() {}
+connectDB()*/
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import mongoose from "mongoose";
-import {DB_NAME} from "./constants";
+//require("dotenv").config({path:'./env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
 
 
+dotenv.config({path: './env'});
 
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("mongo db failed ", error);
+   
+});
 
 
 
